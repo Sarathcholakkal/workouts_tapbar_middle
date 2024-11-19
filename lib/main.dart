accf0cff929b6286344b6cb471540f3965f51e69
sarath <ssarathcholakkall@gmail>
2024-11-19 20:16:09 +0530

    fist commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool status1 = false;
+  bool status2 = true;
+  bool status3 = false;
+  bool status4 = false;
+  bool status5 = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        textTheme: const TextTheme(),
+      ),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: const SafeArea(child: profilePage()),
+      ),
+    );
+  }
+}
+
+class profilePage extends StatefulWidget {
+  const profilePage({super.key});
+
+  @override
+  profilePageState createState() => profilePageState();
+}
+
+class profilePageState extends State<profilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'My Profile',
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.grey[700],
+          elevation: 0,
+          // give the app bar rounded corners
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20.0),
+              bottomRight: Radius.circular(20.0),
+            ),
+          ),
+          leading: const Icon(
+            Icons.menu,
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            // construct the profile details widget here
+            const SizedBox(
+              height: 180,
+              child: Center(
+                child: Text(
+                  'Profile Details Goes here',
+                ),
+              ),
+            ),
+
+            // the tab bar with two items
+            SizedBox(
+              height: 50,
+              child: AppBar(
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.directions_bike),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.directions_car,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // create widgets for each tab bar here
+            Expanded(
+              child: TabBarView(
+                children: [
+                  // first tab bar view widget
+                  Container(
+                    color: Colors.red,
+                    child: const Center(
+                      child: Text(
+                        'Bike',
+                      ),
+                    ),
+                  ),
+
+                  // second tab bar viiew widget
+                  Container(
+                    color: Colors.pink,
+                    child: const Center(
+                      child: Text(
+                        'Car',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
